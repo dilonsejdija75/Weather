58fae7e26c3ad6725cf4d9827e29906f826fbb79
dilonsejdija75 <dilonsejdija75@gmail.com>
2026-06-06 16:37:07 +0100

    Update weatherApi.js

--- frontend/src/lib/weatherApi.js
+++ frontend/src/lib/weatherApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_API_URL;
-export const API = `${BACKEND_URL}/api`;
+export const API = BACKEND_URL;
 
 export const api = axios.create({ baseURL: API, timeout: 30000 });
 
